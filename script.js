4b4c2cb989e76c55ed7c1fb31eb2a2278a1c5d90
Yasin Elhilo <elhiloyasin@gmail.com>
2020-12-14 16:49:34 -0500

    added changes for time

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+$('.btn-info').on('click', function () {  
+let text = $(this).parent().siblings('.col-10').children().val()
+let time = $(this).attr('id')
+console.log(time);
+console.log(text);
+localStorage.setItem(time,text)
+})
+
+
+$('#9').val(localStorage.getItem('9'));
+$('#10').val(localStorage.getItem('10'));
+$('#11').val(localStorage.getItem('11'));
+$('#12').val(localStorage.getItem('12'));
+$('#13').val(localStorage.getItem('13'));
+$('#14').val(localStorage.getItem('14'));
+$('#15').val(localStorage.getItem('15'));
+$('#16').val(localStorage.getItem('16'));
+$('#17').val(localStorage.getItem('17'));
+$('#18').val(localStorage.getItem('18'));
+
+$('textarea').each(function(){
+let blockHour = parseInt($(this).attr('id'))
+let currentHour = 13 
+if(blockHour === currentHour){
+    $(this).addClass('present');
+  
+}
+
+else if(blockHour < currentHour){
+    $(this).addClass('past')
+
+
+}
+
+else if (blockHour > currentHour){
+    $(this).addClass('future')
+
+
+}
+
+
+})
+
+
+
+
+
+
+
+
 
 
 
